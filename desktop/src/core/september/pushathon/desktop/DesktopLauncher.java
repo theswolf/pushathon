3495320a1c4df4bbcf88fe8c0ab9b95c49416920
theswolf <chris.geymo@gmail.com>
2014-05-15 09:17:08 +0200

    initial commit

---
+++ desktop/src/core/september/pushathon/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package core.september.pushathon.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import core.september.pushathon.PushathonGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PushathonGame(), config);
+	}
+}
---
+++ ios/src/core/september/pushathon/IOSLauncher.java
@@ -0,0 +1,22 @@
+package core.september.pushathon;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import core.september.pushathon.PushathonGame;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new PushathonGame(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
