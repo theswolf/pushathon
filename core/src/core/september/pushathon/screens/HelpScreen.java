60d25605e59f283844e50be5b46b28e89a57839b
theswolf <chris.geymo@gmail.com>
2014-06-04 15:54:37 +0200

    Starting help ui

---
+++ core/src/core/september/pushathon/screens/HelpScreen.java
@@ -0,0 +1,72 @@
+package core.september.pushathon.screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+
+import core.september.foundation.AbstractGameScreen;
+import core.september.foundation.util.Constants;
+import core.september.foundation.util.GamePreferences;
+import core.september.pushathon.workers.GameController;
+import core.september.pushathon.workers.GameRenderer;
+
+
+public class HelpScreen extends AbstractGameScreen {
+
+	private GameController gameController;
+	private GameRenderer gameRenderer;
+
+	private boolean paused;
+
+
+	public HelpScreen (Game game) {
+		super(game);
+	}
+
+	@Override
+	public void render (float deltaTime) {
+		// Do not update game world when paused.
+		if (!paused && gameController.timeLeft > Constants.TIME_GONE) {
+			// Update game world by the time that has passed
+			// since last rendered frame.
+			gameController.update(deltaTime);
+		}
+		// Sets the clear screen color to: Cornflower Blue
+	
+		// Render game world to screen
+		gameRenderer.render();
+	}
+
+	@Override
+	public void resize (int width, int height) {
+		super.resize(width, height);
+		gameRenderer.resize(width, height);
+	}
+
+	@Override
+	public void show () {
+		GamePreferences.instance.load();
+		gameController = new GameController(game);
+		gameRenderer = new GameRenderer(gameController);
+		Gdx.input.setCatchBackKey(true);
+	}
+
+	@Override
+	public void hide () {
+		gameRenderer.dispose();
+		Gdx.input.setCatchBackKey(false);
+	}
+
+	@Override
+	public void pause () {
+		paused = true;
+	}
+
+	@Override
+	public void resume () {
+		super.resume();
+		// Only called on Android!
+		paused = false;
+	}
+
+}
---
+++ core/src/core/september/pushathon/workers/HelpRenderer.java
@@ -0,0 +1,273 @@
+/*******************************************************************************
+ * Copyright 2013 Andreas Oehlke
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+
+package core.september.pushathon.workers;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Disposable;
+
+import core.september.foundation.Assets;
+import core.september.foundation.util.Constants;
+
+
+public class HelpRenderer implements Disposable {
+
+	private static final String TAG = HelpRenderer.class.getName();
+
+	private OrthographicCamera camera;
+	private OrthographicCamera helpCamera;
+	private SpriteBatch upBatch;
+	private SpriteBatch downBatch;
+	private ShapeRenderer shadow;
+	private ShapeRenderer shape;
+	private GameController gameController;
+	private Rectangle bounds,touchpoint,viewport;
+	public Vector2 touchBounds;
+	float scale = 1f;
+	boolean increasable = true;
+	
+
+
+	public HelpRenderer (GameController gameController) {
+		this.gameController = gameController;
+		init();
+	}
+
+	private void init () {
+		upBatch = new SpriteBatch();
+		downBatch = new SpriteBatch();
+		shadow = new ShapeRenderer();
+		shape = new ShapeRenderer();
+		camera = new OrthographicCamera(Constants.VIEWPORT_WIDTH, Constants.VIEWPORT_HEIGHT);
+		helpCamera = new OrthographicCamera(Constants.VIEWPORT_WIDTH, Constants.VIEWPORT_HEIGHT); 
+		helpCamera.setToOrtho(true);
+		
+	}
+
+//	public void render () {
+//		renderGame(batch);
+//	}
+	
+    public void render()
+    {
+        
+    	//Gdx.gl.glClearColor(0x64 / 255.0f, 0x95 / 255.0f, 0xed / 255.0f, 0xff / 255.0f);
+    	Gdx.gl.glClearColor(Color.LIGHT_GRAY.r,Color.LIGHT_GRAY.g,Color.LIGHT_GRAY.b,Color.LIGHT_GRAY.a);
+    	 Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		// Clears the screen
+    	// update camera
+		
+		camera.position.set(Gdx.app.getGraphics().getWidth()/2, Gdx.app.getGraphics().getHeight()/2, 0);
+        camera.update();
+
+        // set viewport
+        Gdx.gl.glViewport((int) viewport.x, (int) viewport.y,
+                          (int) viewport.width, (int) viewport.height);
+
+        // clear previous frame
+       
+        renderGame(batch);
+        renderMask(shape);
+        // DRAW EVERYTHING
+    }
+
+    private void renderDown(SpriteBatch batch,int level) {}
+    
+    private void renderUp(SpriteBatch batch) {}
+    
+	private void renderGame (SpriteBatch batch) {
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		//gameController.level.render(batch);
+		//renderBack(batch);
+		bounds = gameController.resources.started? gameController.resources.button.getScaled(scale) :
+			gameController.resources.powerButton.getScaled(scale);
+		renderPowerUnit(batch);
+		renderButton(batch);
+		renderCounter(batch);
+		renderScorer(batch);
+		//renderText(batch);
+		
+		batch.end();
+	}
+	
+	
+	private void renderMask (ShapeRenderer shape) {
+		shape.setProjectionMatrix(camera.combined);
+		shape.begin(ShapeType.Line);
+		shape.setColor(Color.DARK_GRAY);
+		Rectangle boundsD = gameController.resources.counterD.getScaled(scale);
+		shape.rect(boundsD.x,boundsD.y,boundsD.width*2,boundsD.height);
+		boundsD = gameController.resources.scoreU.getScaled(scale);
+		shape.rect(boundsD.x,boundsD.y,boundsD.width*4,boundsD.height);
+		
+		Gdx.gl.glLineWidth(5);
+		shape.end();
+	}
+	
+	
+	
+
+	private void renderPowerUnit(SpriteBatch batch) {
+		TextureRegion  reg = null;
+		reg = gameController.resources.started ? gameController.resources.powerButton.on :  gameController.resources.powerButton.off;
+		Rectangle myBounds = gameController.resources.powerButton.getScaled(scale);
+		batch.draw(reg, 
+				myBounds.x,
+				myBounds.y,
+				myBounds.width,
+				myBounds.height
+				);
+		TextureRegion ledReg =  gameController.resources.started ? gameController.resources.powerLed.on : gameController.resources.powerLed.off;
+		myBounds = gameController.resources.powerLed.getScaled(scale);
+		
+		batch.draw(ledReg, 
+				myBounds.x,
+				myBounds.y,
+				myBounds.width,
+				myBounds.height
+				);
+		
+		batch.setColor(1, 1, 1, 1);
+	}
+	
+	private void renderButton(SpriteBatch batch) {
+		TextureRegion  reg = null;
+		reg = gameController.resources.button.up;
+		Rectangle myBounds = gameController.resources.button.getScaled(scale);
+		batch.draw(reg, 
+				myBounds.x,
+				myBounds.y,
+				myBounds.width,
+				myBounds.height
+				);
+		
+		batch.setColor(1, 1, 1, 1);
+	}
+	
+
+	
+	private void renderCounter(SpriteBatch batch) {
+
+		Rectangle boundsU = gameController.resources.counterU.getScaled(scale);
+		Rectangle boundsD = gameController.resources.counterD.getScaled(scale);
+		
+		batch.draw(gameController.resources.counterU.selected,
+				boundsU.x,
+				boundsU.y,
+				boundsU.width,
+				boundsU.height
+				);
+		
+		batch.draw(gameController.resources.counterD.selected,
+				boundsD.x,
+				boundsD.y,
+				boundsD.width,
+				boundsD.height
+				);
+		
+		batch.setColor(1, 1, 1, 1);
+	}
+	
+	private void renderScorer(SpriteBatch batch) {
+
+		Rectangle boundsU = gameController.resources.scoreU.getScaled(scale);
+		Rectangle boundsD = gameController.resources.scoreD.getScaled(scale);
+		Rectangle boundsM = gameController.resources.scoreM.getScaled(scale);
+		Rectangle boundsDM = gameController.resources.scoreDM.getScaled(scale);
+		
+		batch.draw(gameController.resources.scoreU.selected,
+				boundsU.x,
+				boundsU.y,
+				boundsU.width,
+				boundsU.height
+				);
+		
+		batch.draw(gameController.resources.scoreD.selected,
+				boundsD.x,
+				boundsD.y,
+				boundsD.width,
+				boundsD.height
+				);
+		
+		batch.draw(gameController.resources.scoreM.selected,
+				boundsM.x,
+				boundsM.y,
+				boundsM.width,
+				boundsM.height
+				);
+		batch.draw(gameController.resources.scoreDM.selected,
+				boundsDM.x,
+				boundsDM.y,
+				boundsDM.width,
+				boundsDM.height
+				);
+		
+		batch.setColor(1, 1, 1, 1);
+	}
+
+	public void resize (int width, int height) {
+
+//		camera.viewportWidth = (Constants.VIEWPORT_HEIGHT / (float)height) * (float)width;
+//		camera.
+//		camera.update();
+		
+		float ASPECT_RATIO = Constants.VIEWPORT_WIDTH/Constants.VIEWPORT_HEIGHT;
+		float aspectRatio = (float)width/(float)height;
+        
+        Vector2 crop = new Vector2(0f, 0f);
+        if(aspectRatio > ASPECT_RATIO)
+        {
+            scale = (float)height/(float)Constants.VIEWPORT_HEIGHT ;
+            crop.x = (width - Constants.VIEWPORT_WIDTH *scale)/2f;
+        }
+        else if(aspectRatio < ASPECT_RATIO)
+        {
+            scale = (float)width/(float)Constants.VIEWPORT_WIDTH;
+            crop.y = (height - Constants.VIEWPORT_HEIGHT*scale)/2f;
+        }
+        else
+        {
+            scale = (float)width/(float)Constants.VIEWPORT_WIDTH;
+        }
+
+        float w = (float)Constants.VIEWPORT_WIDTH*scale;
+        float h = (float)Constants.VIEWPORT_HEIGHT*scale;
+        viewport = new Rectangle(crop.x, crop.y, w, h);
+        camera.viewportHeight = h;
+        camera.viewportWidth = w;
+	}
+
+	@Override
+	public void dispose () {
+		batch.dispose();
+		shape.dispose();
+	}
+
+}
