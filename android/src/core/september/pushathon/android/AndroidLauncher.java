3c134650009468f193d317e8664427530d14d8ca
theswolf <chris.geymo@gmail.com>
2014-07-30 17:02:42 +0200

    Glitches

--- android/src/core/september/pushathon/android/AndroidLauncher.java
+++ android/src/core/september/pushathon/android/AndroidLauncher.java
@@ -1,10 +1,19 @@
 package core.september.pushathon.android;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
 import com.google.android.gms.games.Games;
 
 import core.september.foundation.ActionResolver;
@@ -15,7 +24,10 @@ import core.september.pushathon.android.play.GameHelper.GameHelperListener;
 public class AndroidLauncher extends AndroidApplication implements ActionResolver{
 	
 	protected GameHelper mHelper;
-	
+	private static final String AD_UNIT_ID =  "ca-app-pub-0131970786644605/3354463465";
+	  private static final String GOOGLE_PLAY_URL = "https://play.google.com/store/apps/developer?id=TheInvader360";
+	  protected AdView adView;
+	  protected View gameView;
 	
 	private enum LeadebordsID {
 		TOP_PUSHBOXER( "CgkI-OqqhLMdEAIQAQ");
@@ -67,10 +79,67 @@ public class AndroidLauncher extends AndroidApplication implements ActionResolve
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		config.useAccelerometer = false;
 		config.useCompass = false;
+//		
+//		initialize(new PushathonGame(this), config);
+//		initHelper(false);
+//		
+//		AdView adView = (AdView) this.findViewById(R.id.adView);
+//	    AdRequest adRequest = new AdRequest.Builder().build();
+//	    adView.loadAd(adRequest);
 		
-		initialize(new PushathonGame(this), config);
-		initHelper(false);
+	    requestWindowFeature(Window.FEATURE_NO_TITLE);
+	    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+	    getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+
+	    RelativeLayout layout = new RelativeLayout(this);
+	    RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+	    layout.setLayoutParams(params);
+
+	    View gameView = createGameView(config);
+	    AdView admobView = createAdView();
+	    layout.addView(gameView);
+	    layout.addView(admobView);
+	    
+	   
+
+	    setContentView(layout);
+	    initHelper(false);
+	    startAdvertising(admobView);
+
 	}
+	
+	private AdView createAdView() {
+	    adView = new AdView(this);
+	    adView.setAdSize(AdSize.SMART_BANNER);
+	    adView.setAdUnitId(AD_UNIT_ID);
+	    adView.setId(12345); // this is an arbitrary id, allows for relative positioning in createGameView()
+	    RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+//	    params.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
+//	    params.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE);
+	    params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+	    params.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE);
+	    params.addRule(RelativeLayout.BELOW, gameView.getId());
+	    adView.setLayoutParams(params);
+	    adView.setBackgroundColor(Color.BLACK);
+	    return adView;
+	  }
+
+	  private View createGameView(AndroidApplicationConfiguration cfg) {
+	    gameView = initializeForView(new PushathonGame(this), cfg);
+	    RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+	    params.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
+	    params.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE);
+//	    params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+//	    params.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE);
+//	    params.addRule(RelativeLayout.BELOW, adView.getId());
+	    gameView.setLayoutParams(params);
+	    return gameView;
+	  }
+	  
+	  private void startAdvertising(AdView adView) {
+		    AdRequest adRequest = new AdRequest.Builder().build();
+		    adView.loadAd(adRequest);
+		  }
 	
 	private void initHelper(boolean connectOnStart) {
 	    mHelper = new GameHelper(this, GameHelper.CLIENT_GAMES);
@@ -127,6 +196,24 @@ public class AndroidLauncher extends AndroidApplication implements ActionResolve
 		super.onStop();
 		mHelper.onStop();
 	}
+	
+	 @Override
+	  public void onResume() {
+	    super.onResume();
+	    if (adView != null) adView.resume();
+	  }
+
+	  @Override
+	  public void onPause() {
+	    if (adView != null) adView.pause();
+	    super.onPause();
+	  }
+
+	  @Override
+	  public void onDestroy() {
+	    if (adView != null) adView.destroy();
+	    super.onDestroy();
+	  }
 	
 	@Override
 	protected void onActivityResult(int request, int response, Intent data) {
